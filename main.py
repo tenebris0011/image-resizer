fe99147a4497d70bb5175a1084c0ed9a8663ba50
Nathanial Wilson <wilson.nathanial95@gmail.com>
2022-12-24 17:00:39 -0600

    Adding session state

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import streamlit as st
 from PIL import Image
 import os
 import shutil
+import random
+import string
 
 def resize_images(uploaded):
     if not os.path.exists('./resized'):
         os.makedirs('./resized')
-    if os.path.exists('./resized_images.zip'):
-        os.remove('./resized_images.zip')
     file_list = []
     for uploaded_file in uploaded_files:
         file_list.append(uploaded_file.name)
@@ -29,9 +29,8 @@ def resize_images(uploaded):
             resized_im = im.resize((round(im.size[0]*size), round(im.size[1]*size)))
             resized_im.save("./resized/" + new_file_name)
             resized_im.save("./resized/" + new_file_name + ".webp", format="webp")
-    shutil.make_archive("resized_images", 'zip', "./resized")
-    shutil.rmtree('./images/')
-    shutil.rmtree('./resized/')
+    shutil.make_archive(st.session_state['session_key'], 'zip', "./resized")
+    st.session_state['session_zip_file'] = st.session_state['session_key'] + ".zip"
 
 
 def save_uploadedfile(uploadedfile):
@@ -46,16 +45,22 @@ def save_uploadedfile(uploadedfile):
 uploaded = []
 st.title("Image Resizer")
 
+if 'session_key' not in st.session_state:
+    st.session_state['session_key'] = ''.join(random.choices(string.ascii_lowercase, k=5))
+    st.session_state['session_zip_file'] = st.session_state['session_key'] + ".zip"
+
 uploaded_files = st.file_uploader("Choose a image file", accept_multiple_files=True)
+
 for uploaded_file in uploaded_files:
     file_path = save_uploadedfile(uploaded_file)
 
 st.button('Resize', key=None, help=None, on_click=resize_images, args=(uploaded_files,), disabled=False)
-if os.path.exists('./resized_images.zip'):
-    with open("resized_images.zip", "rb") as fp:
+
+if os.path.exists(st.session_state['session_zip_file']):
+    with open(st.session_state['session_zip_file'], "rb") as fp:
         btn = st.download_button(
             label="Download Resized Images ZIP",
             data=fp,
-            file_name="resized_images.zip",
+            file_name=st.session_state['session_zip_file'],
             mime="application/zip"
         )
